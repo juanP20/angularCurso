cd212ab22aba737e151dec8100d8ddeb7c834ef5
Juan Pablo Arenas Durango <juan.arenas@premexcorp.com>
2024-05-03 08:18:01 -0500

    My first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular-youtube';
+  title = 'HOLA MUNDO  ANGULAR ';
+  subtitle =  'Hola interpolar '
 }
